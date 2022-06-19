52f218592103069d072a41f930dbe1a356e99489
math3r0se <tordjmanjoel@outlook.com>
2022-06-19 15:19:42 +0200

    Pipou

---
+++ its-ok/README.md
@@ -0,0 +1,3 @@
+# It's Ok
+
+Juste pour te rappeler que tout ira bien <3
---
+++ its-ok/itsok.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){
+
+    char a[10];
+
+    printf("Coucou toi !\n");
+    printf("Bah alors, t'a mauvaise mine, qu'est-ce qui ne va pas ?\n\n-> ");
+    scanf("%s", a);
+
+    printf("\nNe t'inquietes pas, tout ira bien, prends soin de toi <3.\n");
+
+    return 0;
+}
