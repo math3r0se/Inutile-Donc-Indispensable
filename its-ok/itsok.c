1f064a8d1873c823f2be6a7624f65528bc781d3a
math3r0se <tordjmanjoel@outlook.com>
2022-06-19 15:20:43 +0200

    Ne pas oublier de croire

--- its-ok/itsok.c
+++ its-ok/itsok.c
@@ -8,7 +8,7 @@ int main(){
     printf("Bah alors, t'a mauvaise mine, qu'est-ce qui ne va pas ?\n\n-> ");
     scanf("%s", a);
 
-    printf("\nNe t'inquietes pas, tout ira bien, prends soin de toi <3.\n");
+    printf("\nNe t'inquietes pas, tout ira bien, moi je crois en toi !\nPrends soin de toi <3.\n");
 
     return 0;
 }
